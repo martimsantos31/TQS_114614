f8d99c420783a837e53cbe55f22b6f4325287946
martimsoutooo <martimsantos@aluno-1793.wireless.ua.pt>
2025-03-22 19:17:46 +0000

    ex1 done ig

---
+++ lab5/lab5_1Calculator/src/test/java/pt/ua/deti/tqs/CalculatorSteps.java
@@ -0,0 +1,78 @@
+package pt.ua.deti.tqs;/*
+ * (C) Copyright 2017 Boni Garcia (https://bonigarcia.github.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+import static java.lang.invoke.MethodHandles.lookup;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.slf4j.LoggerFactory.getLogger;
+
+import org.slf4j.Logger;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class CalculatorSteps {
+
+    static final Logger log = getLogger(lookup().lookupClass());
+
+    private Calculator calc;
+
+    @Given("^a calculator I just turned on$")
+    public void setup() {
+        calc = new Calculator();
+    }
+
+    @When("I add {int} and {int}")
+    public void add(int arg1, int arg2) {
+        log.debug("Adding {} and {}", arg1, arg2);
+        calc.push(arg1);
+        calc.push(arg2);
+        calc.push("+");
+    }
+
+    @When("I substract {int} from {int}")
+    public void substract(int arg1, int arg2) {
+        log.debug("Substracting {} and {}", arg1, arg2);
+        calc.push(arg1);
+        calc.push(arg2);
+        calc.push("-");
+    }
+
+    @When("I multiply {int} by {int}")
+    public void multiply(int arg1, int arg2) {
+        log.debug("Multiplying {} and {}", arg1, arg2);
+        calc.push(arg1);
+        calc.push(arg2);
+        calc.push("*");
+    }
+
+    @When("I divide {int} by {int}")
+    public void divide(int arg1, int arg2) {
+        log.debug("Dividing {} and {}", arg1, arg2);
+        calc.push(arg1);
+        calc.push(arg2);
+        calc.push("/");
+    }
+
+    @Then("the result is {double}")
+    public void the_result_is(double expected) {
+        Number value = calc.value();
+        log.debug("Result: {} (expected {})", value, expected);
+        assertEquals(expected, value);
+    }
+
+}
